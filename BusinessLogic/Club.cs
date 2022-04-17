aaff053d473443cadeae78ef81ba7ff0abb604ae
Taras Kozodoi <acateely@gmail.com>
2022-04-17 18:27:33 +0300

    first commit

---
+++ BusinessLogic/Club.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    abstract class Club
+    {
+        public abstract bool Visit();
+        public abstract bool Visit(Cart cart);
+        public abstract Cart BuyClubCart();
+        public abstract Cart BuySpecialCart();
+        public abstract Cart SingUp();
+        public abstract bool SingUp(Cart cart);
+    }
+}
