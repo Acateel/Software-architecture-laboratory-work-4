6b124e04943f1de7fc01f45a16e96c6f9ae10b2d
Taras Kozodoi <acateely@gmail.com>
2022-05-02 10:26:47 +0300

    added folder for carts and clubs

--- BusinessLogic/Carts/Cart.cs
+++ BusinessLogic/Carts/Cart.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using BusinessLogic.TimeTables;
 
-namespace BusinessLogic
+namespace BusinessLogic.Carts
 {
     public abstract class Cart
     {
--- BusinessLogic/Clubs/Club.cs
+++ BusinessLogic/Clubs/Club.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BusinessLogic.TimeTables;
+using BusinessLogic.Timer;
+using BusinessLogic.Carts;
 
-namespace BusinessLogic
+namespace BusinessLogic.Clubs
 {
     abstract class Club
     {
+        protected ITimeTable table;
+
+        protected Club(ITimeTable table)
+        {
+            this.table = table;
+        }
+
         public abstract bool Visit();
         public abstract bool Visit(Cart cart);
         public abstract Cart BuyClubCart();
