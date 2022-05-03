96d258850b56c53126403f76fd1e35f66ff8fa4d
Taras Kozodoi <acateely@gmail.com>
2022-05-03 14:19:16 +0300

    Added CartRepository and ClubRepository

---
+++ UnitOfWork/Services/CartRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Carts;
+using UnitOfWork.Interfaces;
+
+namespace UnitOfWork.Services
+{
+    class CartRepository : Repository<Cart>, ICartRepository
+    {
+        public CartRepository(ClubsContext context) : base(context) { }
+
+        public IQueryable<Cart> GetAll()
+        {
+            try
+            {
+                return Db.Carts;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("GetAll() failed: {0}", ex);
+                throw;
+            }
+        }
+
+        public Cart GetCart(long id)
+        {
+            try
+            {
+                return Db.Carts.FirstOrDefault(c => c.Id == id);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("GetClub() failed: {0}", ex);
+                throw;
+            }
+        }
+    }
+}
---
+++ UnitOfWork/Services/ClubRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Clubs;
+using UnitOfWork.Interfaces;
+
+namespace UnitOfWork.Services
+{
+    public class ClubRepository : Repository<Club>, IClubRepository
+    {
+        public ClubRepository(ClubsContext context) : base(context) { }
+
+        public IQueryable<Club> GetAll()
+        {
+            try
+            {
+                return Db.Clubs;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("GetAll() failed: {0}", ex);
+                throw;
+            }
+        }
+
+        public Club GetClub(long id)
+        {
+            try
+            {
+                return Db.Clubs.FirstOrDefault(c => c.Id == id);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("GetClub() failed: {0}", ex);
+                throw;
+            }
+        }
+    }
+}
