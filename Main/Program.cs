5172a4df17eeee06acbf2081e685ff4b32cbe1b6
Taras Kozodoi <acateely@gmail.com>
2022-05-03 13:35:48 +0300

    added testing main

--- Main/Program.cs
+++ Main/Program.cs
@@ -4,7 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Entities.TimeTables;
+using Entities.Clubs;
+using Entities.Carts;
+using Entities;
 using System.Data.Entity;
+using UnitOfWork;
 
 namespace Main
 {
@@ -12,7 +16,24 @@ namespace Main
     {
         static void Main(string[] args)
         {
-            
+            ClubsContext context = new ClubsContext();
+
+            Club club = new LocClub("Default", "DefaultCity", new TimeTable());
+
+            context.Clubs.Add(club);
+        }
+        static void ShowContext(ClubsContext context)
+        {
+            Console.WriteLine("Carts:");
+            foreach (var entity in context.Carts.Local)
+            {
+                Console.WriteLine(entity.ToString());
+            }
+            Console.WriteLine("Clubs:");
+            foreach (var entity in context.Clubs.Local)
+            {
+                Console.WriteLine(entity.ToString());
+            }
         }
     }
 }
