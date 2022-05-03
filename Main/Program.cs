31a15c7a23309fc634e63f23a9c1da37c90abcaf
Taras Kozodoi <acateely@gmail.com>
2022-05-03 13:27:09 +0300

    added entity framewort to Main project

--- Main/Program.cs
+++ Main/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Entities.TimeTables;
+using System.Data.Entity;
 
 namespace Main
 {
@@ -11,12 +12,7 @@ namespace Main
     {
         static void Main(string[] args)
         {
-            ITimeTable time = new TimeTable();
-            time.SetConsonant(19, 21);
-            Console.WriteLine(time.IsTimeFree(18));
-            Console.WriteLine(time.IsTimeFree(19));
-            Console.WriteLine(time.IsTimeFree(21));
-            Console.WriteLine(time.IsTimeFree(22));
+            
         }
     }
 }
