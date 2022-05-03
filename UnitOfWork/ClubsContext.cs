8de089628cbb0cbbaabf130744f8decfa5bb3f07
Taras Kozodoi <acateely@gmail.com>
2022-05-03 12:23:35 +0300

    Added entity framework

---
+++ UnitOfWork/ClubsContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using Entities.Clubs;
+using Entities.Carts;
+
+namespace UnitOfWork
+{
+    public class ClubsContext : DbContext
+    {
+        public ClubsContext() : base ("ProductDBConnectionString")
+        {
+        }
+
+        public DbSet<Club> Clubs { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+
+        static ClubsContext()
+        {
+            Database.SetInitializer<ClubsContext>(new CreateDatabaseIfNotExists<ClubsContext>());
+        }
+
+
+    }
+}
