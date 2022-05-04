b111c550cc579d4f786dd23e5a46522f31edd79f
Taras Kozodoi <acateely@gmail.com>
2022-05-04 09:12:17 +0300

    Added club logic

---
+++ BusinessLogic/Servises/ClubLogic.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Carts;
+using Entities.TimeTables;
+using BusinessLogic.Interfaces;
+using UnitOfWork.Interfaces;
+using Entities.Clubs;
+
+namespace BusinessLogic.Servises
+{
+    class ClubLogic : IClubLogic
+    {
+        public Club Club { get; set; }
+
+        public Cart BuyClubCart(ITimeTable timeTable)
+        {
+            return Club.BuyClubCart(timeTable);
+        }
+
+        public Cart BuySpecialCart(ITimeTable timeTable)
+        {
+            return Club.BuySpecialCart(timeTable);
+        }
+
+        public void ChangeInfo(Club club)
+        {
+            Club = club;
+        }
+
+        public string GetClubInfo()
+        {
+            return Club.ToString();
+        }
+
+        public Cart SingUp(int time)
+        {
+            return Club.SingUp(time);
+        }
+
+        public bool SingUp(Cart cart, int time)
+        {
+            return Club.SingUp(cart, time);
+        }
+
+        public bool VisitClub()
+        {
+            Club.Visit();
+            return false;
+        }
+
+        public bool VisitClub(Cart cart)
+        {
+            return Club.Visit(cart);
+        }
+    }
+}
