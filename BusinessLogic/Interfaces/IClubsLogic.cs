f9f90fc6a2006867ab7dc58eebcb95fb237c72cb
Taras Kozodoi <acateely@gmail.com>
2022-05-04 09:29:28 +0300

    Added clubs logic

--- BusinessLogic/Interfaces/IClubsLogic.cs
+++ BusinessLogic/Interfaces/IClubsLogic.cs
@@ -10,7 +10,7 @@ namespace BusinessLogic.Interfaces
     interface IClubsLogic
     {
         IQueryable<Club> GetClubs();
-        void SetClub(int id);
+        Club SetClub(int id);
         void CreateClub(Club club);
         void DeleteClub(int id);
     }
---
+++ BusinessLogic/Servises/ClubsLogic.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Carts;
+using Entities.TimeTables;
+using BusinessLogic.Interfaces;
+using UnitOfWork.Interfaces;
+using Entities.Clubs;
+
+namespace BusinessLogic.Servises
+{
+    class ClubsLogic : IClubsLogic
+    {
+        private readonly IClubRepository repository;
+
+        public ClubsLogic(IClubRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public void CreateClub(Club club)
+        {
+            repository.Insert(club);
+        }
+
+        public void DeleteClub(int id)
+        {
+            repository.Delete(id);
+        }
+
+        public IQueryable<Club> GetClubs()
+        {
+            return repository.GetAll();
+        }
+
+        public Club SetClub(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
