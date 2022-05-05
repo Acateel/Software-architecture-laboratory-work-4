d94fefb2eaa6c5cc85259bc2f6591f6128fab064
Taras Kozodoi <acateely@gmail.com>
2022-05-05 10:11:54 +0300

    added presentation interfaces

---
+++ Presentation/Interfaces/ICartMenu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.Interfaces
+{
+    interface ICartMenu
+    {
+        void GetCarts();
+        void GetTimeTable();
+
+        void ChangeTimeTable();
+
+        void RemoveCart();
+    }
+}
---
+++ Presentation/Interfaces/IClubMenu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.Interfaces
+{
+    interface IClubMenu
+    {
+        void GetClubInfo();
+        void ChangeInfo();
+        void VisitClub();
+
+        void BuyClubCart();
+        void BuySpecialCart();
+
+        void SingUp();
+    }
+}
