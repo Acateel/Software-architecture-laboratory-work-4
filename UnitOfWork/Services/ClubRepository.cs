75c5a97d912c0bd8a6454b677389492c31e990e3
Taras Kozodoi <acateely@gmail.com>
2022-05-10 13:41:50 +0300

    add include TimeTable to ClubRepository

--- UnitOfWork/Services/ClubRepository.cs
+++ UnitOfWork/Services/ClubRepository.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Entities.Clubs;
+using Entities.TimeTables;
 using UnitOfWork.Interfaces;
 
 namespace UnitOfWork.Services
@@ -35,6 +37,33 @@ namespace UnitOfWork.Services
             {
                 Console.WriteLine("GetClub() failed: {0}", ex);
                 throw;
+            }
+        }
+
+        public override void Insert(Club entity)
+        {
+            base.Insert(entity);
+            Db.TimeTables.Add(entity.Table);
+        }
+
+        public override void Update(Club entity)
+        {
+            base.Update(entity);
+            Db.TimeTables.Attach(entity.Table);
+            Db.Entry(entity.Table).State = EntityState.Modified;
+        }
+
+        public override void Delete(int id)
+        {
+            Club entity = Db.Set<Club>().Find(id);
+            TimeTable table = Db.TimeTables.Find(entity.Table_Id);
+            if (entity != null)
+            {
+                Db.Set<Club>().Remove(entity);
+            }
+            if (table != null)
+            {
+                Db.TimeTables.Remove(table);
             }
         }
     }
