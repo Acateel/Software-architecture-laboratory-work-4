e57573abdf3e6af1f5f9c98815d265004efc0cd5
Taras Kozodoi <acateely@gmail.com>
2022-05-12 10:02:12 +0300

    add ClubControler Visit club

--- ClubsAPI/Controllers/ClubController.cs
+++ ClubsAPI/Controllers/ClubController.cs
@@ -22,30 +22,31 @@ namespace ClubsAPI.Controllers
             this.logic = logic;
         }
 
-
-
-        // GET: api/Club
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
         // GET: api/Club/5
-        public string Get(int id)
+        public IHttpActionResult GetVisit(int id, int cartId)
         {
-            return "value";
+            if (!SelectionClub(id))
+            {
+                return BadRequest("Club Id not found");
+            }
+
+            if(cartId == 0)
+            {
+                return VisitWithoutCart();
+            }
+            else
+            {
+                return VisitWithCart(cartId);
+            }
         }
 
         // POST: api/Club
         public IHttpActionResult PostByCart(int id, string type, TimeTable table)
         {
-            try
+
+            if (!SelectionClub(id))
             {
-                logic.Clubs.SetClub(id);
-            }
-            catch (InvalidOperationException)
-            {
-                return BadRequest("Id not found");
+                return BadRequest("Club Id not found");
             }
 
             Cart cart;
@@ -66,34 +67,76 @@ namespace ClubsAPI.Controllers
         // PUT: api/Club/5
         public IHttpActionResult PutSingUp(int id, int cartId, int time)
         {
-            try
-            {
-                logic.Clubs.SetClub(id);
-            }
-            catch (InvalidOperationException)
+            if (!SelectionClub(id))
             {
                 return BadRequest("Club Id not found");
             }
 
-            if(cartId == 0)
+            if (cartId == 0)
             {
                 var cart = logic.Club.SingUp(time);
                 return Ok(cart);
             }
             else
             {
-                Cart cart;
-                try
-                {
-                    cart = logic.Carts.GetCart(cartId);
-                }
-                catch (InvalidOperationException)
-                {
-                    return BadRequest("Cart Id not found");
-                }
-                bool singing = logic.Club.SingUp(cart, time);
-                return Ok(singing);
+                return SingUpWithCart(cartId, time);
             }
+        }
+
+        private bool SelectionClub(int id)
+        {
+            try
+            {
+                logic.Clubs.SetClub(id);
+                return true;
+            }
+            catch (InvalidOperationException)
+            {
+                return false;
+            }
+        }
+
+        private IHttpActionResult SingUpWithCart(int cartId, int time)
+        {
+            Cart cart;
+            try
+            {
+                cart = logic.Carts.GetCart(cartId);
+            }
+            catch (InvalidOperationException)
+            {
+                return BadRequest("Cart Id not found");
+            }
+            bool singing = logic.Club.SingUp(cart, time);
+            return Ok(singing);
+        }
+
+        private IHttpActionResult VisitWithoutCart()
+        {
+            try
+            {
+                logic.Club.VisitClub();
+                return Ok("How you visit club without cart?");
+            }
+            catch(ClubException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        private IHttpActionResult VisitWithCart(int cartId)
+        {
+            Cart cart;
+            try
+            {
+                cart = logic.Carts.GetCart(cartId);
+            }
+            catch (InvalidOperationException)
+            {
+                return BadRequest("Cart Id not found");
+            }
+            bool visiting = logic.Club.VisitClub(cart);
+            return Ok(visiting);
         }
     }
 }
