67b4f87e3570d6ef9ae71909582eebd86f4aaba0
Taras Kozodoi <acateely@gmail.com>
2022-05-12 09:00:33 +0300

    add TimeControlers

---
+++ ClubsAPI/Controllers/TimeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Entities.Timers;
+
+namespace ClubsAPI.Controllers
+{
+    public class TimeController : ApiController
+    {
+        // GET: api/Time
+        public int Get()
+        {
+            return ClasicTimer.GetTime();
+        }
+
+        // PUT: api/Time
+        public IHttpActionResult Put(int time)
+        {
+            ClasicTimer.setTime(time);
+            return Ok(time);
+        }
+    }
+}
