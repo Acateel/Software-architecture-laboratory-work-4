180ea919e186174c3b2fd481caab4a5048023a5d
Taras Kozodoi <acateely@gmail.com>
2022-05-13 12:18:21 +0300

    add unit test project

---
+++ TestingBussinesLogic/TestCartLogic.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Moq;
+using BusinessLogic.Servises;
+using UnitOfWork.Interfaces;
+using Entities.Carts;
+
+namespace TestingBussinesLogic
+{
+    public class TestCartLogic
+    {
+        [Fact]
+        public void GetCarts()
+        {
+            // Arrange
+            List<Cart> carts = new List<Cart>();
+            carts.Add(new ClubCart());
+            carts.Add(new SpecialCart());
+            var mock = new Mock<ICartRepository>();
+            mock.Setup(a => a.GetAll()).Returns(carts.AsQueryable<Cart>());
+            CartLogic cartLogic = new CartLogic(mock.Object);
+            
+            // Act
+            var result = cartLogic.GetCarts();
+            
+            // Assert
+            Assert.Equal(2, result.Count());
+        }
+    }
+}
