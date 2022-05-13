581b9769e0c8b70f8a6db541b9a8bb7722d39fd1
Taras Kozodoi <acateely@gmail.com>
2022-05-13 13:02:19 +0300

    add cart logic tests

--- TestingBussinesLogic/TestCartLogic.cs
+++ TestingBussinesLogic/TestCartLogic.cs
@@ -8,6 +8,7 @@ using Moq;
 using BusinessLogic.Servises;
 using UnitOfWork.Interfaces;
 using Entities.Carts;
+using Entities.TimeTables;
 
 namespace TestingBussinesLogic
 {
@@ -29,6 +30,77 @@ namespace TestingBussinesLogic
             
             // Assert
             Assert.Equal(2, result.Count());
+        }
+
+        [Fact]
+        public void GetCartWihtId()
+        {
+            // Arrange
+            int id = 5;
+            var mock = new Mock<ICartRepository>();
+            CartLogic cartLogic = new CartLogic(mock.Object);
+
+            // Act
+            var cart = cartLogic.GetCart(id);
+
+            // Assert
+            mock.Verify(t => t.Get(id));
+        }
+
+        [Fact]
+        public void ChangeTimeTable()
+        {
+            // Arrange
+            
+            var mock = new Mock<ICartRepository>();
+            Cart cart = new ClubCart();
+            cart.Table = new TimeTable(0, "");
+            TimeTable table = new TimeTable();
+            mock.Setup(a => a.Get(It.IsAny<int>())).Returns(cart);
+            CartLogic cartLogic = new CartLogic(mock.Object);
+
+            // Act
+            cartLogic.ChangeTimeTable(0, table);
+
+            // Assert
+            Assert.Equal(cart.Table.Table, table.Table);
+            mock.Verify(t => t.Update(cart));
+            mock.Verify(t => t.Save());
+        }
+
+        [Fact]
+        public void GetTimeTable()
+        {
+            // Arrange
+
+            var mock = new Mock<ICartRepository>();
+            Cart cart = new ClubCart();
+            TimeTable table = new TimeTable();
+            cart.Table = table;
+            mock.Setup(a => a.Get(It.IsAny<int>())).Returns(cart);
+            CartLogic cartLogic = new CartLogic(mock.Object);
+
+            // Act
+            var result = cartLogic.GetTimeTable(0);
+
+            // Assert
+            Assert.True(result.Equals(table));
+        }
+
+        [Fact]
+        public void RemoveCart()
+        {
+            // Arrange
+            int id = 5;
+            var mock = new Mock<ICartRepository>();
+            CartLogic cartLogic = new CartLogic(mock.Object);
+
+            // Act
+            cartLogic.RemoveCart(id);
+
+            // Assert
+            mock.Verify(t => t.Delete(id));
+            mock.Verify(t => t.Save());
         }
     }
 }
